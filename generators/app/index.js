5975c18de8fc4516f9a4085549590c52b3aa2ee6
hansent <thomas.hansen@gmail.com>
2014-09-08 13:18:08 -0500

    general improovements
    - remove unused dependencies
    - add nicer logging using chalk
    - appname basedon prompt
    - ask whether to run git init and npm install automatically

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,11 +1,44 @@
 'use strict';
 
 var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
 
-var AppstractGenerator = yeoman.generators.NamedBase.extend({
+
+var AppstractGenerator = yeoman.generators.Base.extend({
+
+    promptTask: function() {
+        var done = this.async();
+        this.prompt([
+            {
+                type: 'input',
+                name: 'name',
+                message: 'project name'
+            },
+            {
+                type: 'confirm',
+                name: 'run_git',
+                message: 'run git init? (default: yes)',
+                default: true
+            },
+            {
+                type: 'confirm',
+                name: 'run_npm_install',
+                message: 'run npm install? (default: yes)',
+                default: true
+            },
+
+        ], function(answers) {
+            this.name = answers.name;
+            this.run_git = answers.run_git;
+            this.run_npm_install = answers.run_npm_install
+            done();
+        }.bind(this));
+    },
 
     app: function() {
-        console.log('APP NAME:', this.name);
+        console.log("\ngenerating app files...");
+        console.log('APP NAME:', chalk.bold.green(this.name));
+
         this.appname = this.name;
         this.username = require('git-user-name');
         this.useremail = require('git-user-email');
@@ -25,7 +58,37 @@ var AppstractGenerator = yeoman.generators.NamedBase.extend({
         this.copy('public/main.js', 'public/main.js');
         this.copy('public/style.css', 'public/style.css');
 
-        this.npmInstall()
+    },
+
+    gitinit: function() {
+        var done = this.async();
+
+        if (!this.run_git)
+            return done();
+
+        console.log("\nrunning " + chalk.yellow("git init"));
+        this.spawnCommand('git', ['init'])
+            .on('exit', function(code) {
+                console.log(chalk.green('OK') + '\n');
+                done();
+            });
+    },
+
+
+    end: function() {
+        var done = this.async();
+
+        if (!this.run_npm_install)
+            return done();
+
+        console.log("running " + chalk.yellow("npm install"));
+        this.npmInstall(null, null, function() {
+            console.log(chalk.green("\nALL DONE!"));
+            console.log(chalk.white("you can now run"));
+            console.log(chalk.yellow("cd " + this.appname));
+            console.log(chalk.yellow("gulp"));
+            done();
+        }.bind(this));
     }
 
 });
